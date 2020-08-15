907a475a88e4f4906ca052da79034212d15a290b
seanmars <mars0222@gmail.com>
2020-08-16 03:21:15 +0800

    Add the google calendar function

---
+++ Assets/Scripts/CalendarHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnityEngine.Networking;
+
+namespace CalendarUtility
+{
+    public struct CalendarEvent
+    {
+        public string Title { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int? Duration { get; set; }
+
+        public bool IsAllDayEvent { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public static class CalendarHelper
+    {
+        public static int DefaultDurationMinutes = 60;
+
+        public static string ToIsoString(this DateTime dateTime)
+        {
+            var ios = dateTime.ToString("O");
+            var reg = new Regex("/-|:|\\.\\d+/g");
+            return reg.Replace(ios, string.Empty);
+        }
+
+        public static string CalculateEndTime(CalendarEvent calendarEvent)
+        {
+            var duration = calendarEvent.Duration ?? DefaultDurationMinutes;
+
+            return calendarEvent.EndTime.HasValue
+                ? calendarEvent.EndTime.Value.ToIsoString()
+                : calendarEvent.StartTime.AddMinutes(duration).ToIsoString();
+        }
+
+        public static string GenerateGoogleCalendarUrl(CalendarEvent calendarEvent)
+        {
+            var startTime = calendarEvent.IsAllDayEvent
+                ? calendarEvent.StartTime.ToString("yyyyMMdd")
+                : calendarEvent.StartTime.ToIsoString();
+            var endTime = calendarEvent.IsAllDayEvent
+                ? calendarEvent.StartTime.AddDays(1).ToString("yyyyMMdd")
+                : CalculateEndTime(calendarEvent);
+
+            var url = string.Join("", new[]
+            {
+                "https://www.google.com/calendar/render",
+                "?action=TEMPLATE",
+                $"&text={calendarEvent.Title}",
+                $"&dates={startTime}",
+                $"/{endTime}",
+                $"&details={UnityWebRequest.EscapeURL(calendarEvent.Description)}",
+                "&sprop=&sprop=name:"
+            });
+
+            return url;
+        }
+    }
+}
--- Assets/Scripts/EndManager.cs
+++ Assets/Scripts/EndManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using CalendarUtility;
 using UnityEngine;
 
 public class EndManager : MonoBehaviour
@@ -7,12 +9,34 @@ public class EndManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void OnYesClick()
     {
+        var calendarEvent = new CalendarEvent();
+        calendarEvent.Title = "TEST";
+        calendarEvent.StartTime = DateTime.Now;
+        calendarEvent.IsAllDayEvent = true;
+        calendarEvent.Description = "TEST Description";
 
+        var url = string.Empty;
+        switch (Application.platform)
+        {
+            case RuntimePlatform.tvOS:
+            case RuntimePlatform.IPhonePlayer:
+            case RuntimePlatform.OSXEditor:
+            case RuntimePlatform.OSXPlayer:
+                break;
+
+            case RuntimePlatform.Android:
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+            default:
+                url = CalendarHelper.GenerateGoogleCalendarUrl(calendarEvent);
+                Application.OpenURL(url);
+                break;
+        }
     }
 
     public void OnNoClick()
